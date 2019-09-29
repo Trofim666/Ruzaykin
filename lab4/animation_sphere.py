de68dc7e403e05fca7deabeb830f2dd36425ef45
Trofim666 <ruzaikin.td@phystech.edu>
2019-09-29 17:10:28 +0400

    Add some functions

--- lab4/animation_sphere.py
+++ lab4/animation_sphere.py
@@ -3,6 +3,7 @@ import graphics as gr
 from math import sin
 from math import cos
 from math import pi
+from math import acos
 
 SIZE_X = 1000
 SIZE_Y = 650
@@ -10,11 +11,40 @@ SIZE_Y = 650
 window = gr.GraphWin("OknO", SIZE_X, SIZE_Y)
 window.setBackground('aqua')
 
-my_circle = gr.Circle(gr.Point(500, 325), 300)
-my_circle.draw(window)
-my_circle.setFill('yellow')
-my_circle.setOutline('green')
-my_circle.setWidth(20)
+position = gr.Point(500,325)
+speed = gr.Point(3,3)
 
-window.getMouse()
-window.close()
+x_0 = 500
+y_0 = 325
+
+my_big_circle = gr.Circle(gr.Point(x_0, y_0), 300)
+my_big_circle.draw(window)
+my_big_circle.setFill('yellow')
+my_big_circle.setOutline('green')
+my_big_circle.setWidth(20)
+
+ball = gr.Circle(position, 10)
+ball.draw(window)
+ball.setFill('red')
+
+def go():
+    pass
+
+
+def scalar_multiplication():
+    pass
+
+
+def change_speed():
+    pass
+
+
+def check_position():
+    pass
+
+
+while True:
+    ball.move(speed.x,speed.y)
+
+    
+    gr.time.sleep(0.02)
