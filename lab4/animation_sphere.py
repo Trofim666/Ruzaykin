3b0c125c7e828bb6c368755228e00a0a26e319e2
Trofim666 <ruzaikin.td@phystech.edu>
2019-10-01 16:22:38 +0400

    Fixed version

--- lab4/animation_sphere.py
+++ lab4/animation_sphere.py
@@ -1,28 +1,34 @@
 import graphics as gr
 
 from math import sqrt
-
+                                                                               
 SIZE_X = 1000
 SIZE_Y = 650
 
-window = gr.GraphWin("OknO", SIZE_X, SIZE_Y)
-window.setBackground('aqua')
-
 position = gr.Point(600,310)
-speed = gr.Point(3,7)
+speed = gr.Point(1,7)
 
 x_0 = 500
 y_0 = 325
 
-my_big_circle = gr.Circle(gr.Point(x_0, y_0), 300)
-my_big_circle.draw(window)
-my_big_circle.setFill('yellow')
-my_big_circle.setOutline('green')
-my_big_circle.setWidth(20)
 
-ball = gr.Circle(position, 30)
-ball.draw(window)
-ball.setFill('red')
+window = gr.GraphWin("OknO", SIZE_X, SIZE_Y)
+window.setBackground('aqua')
+
+
+def draw_big_circle():
+    my_big_circle = gr.Circle(gr.Point(x_0, y_0), 300)
+    my_big_circle.draw(window)
+    my_big_circle.setFill('green')
+    my_big_circle.setOutline('yellow')
+    my_big_circle.setWidth(20)
+    
+
+def draw_ball():
+    ball = gr.Circle(position, 30)
+    ball.draw(window)
+    ball.setFill('red')
+    return ball
 
 
 def add(point_1, point_2):
@@ -31,12 +37,13 @@ def add(point_1, point_2):
     return new_point
 
 
-def ball_go():
+def ball_go(ball):
     ball.move(speed.x, speed.y)
 
 
-def scalar_x_vector(scalar,vector):
-    new_vector = gr.Point( scalar*vector.x, scalar*vector.y )
+
+def scalar_x_vector(scalar, vector):
+    new_vector = gr.Point(scalar*vector.x, scalar*vector.y)
     
     return new_vector
 
@@ -48,13 +55,23 @@ def scalar_multiplication(vector_1, vector_2):
 
 
 def changed_speed(speed, x, y):
-    speed_along_r = scalar_x_vector (scalar_multiplication (vector_along_radius(x,y), speed),
-                                    vector_along_radius(x,y))
+    speed_along_r = scalar_x_vector(
+        scalar_multiplication(
+            vector_along_radius(x, y),
+            speed
+        ),
+        vector_along_radius(x,y)
+    )
     
-    speed_normal_r = scalar_x_vector (scalar_multiplication (vector_normal_radius(x,y), speed),
-                                     vector_normal_radius(x,y))
+    speed_normal_r = scalar_x_vector(
+        scalar_multiplication(
+            vector_normal_radius(x, y),
+            speed
+        ),
+        vector_normal_radius(x, y)
+    )
     
-    speed_along_r_new = scalar_x_vector (-1, speed_along_r)
+    speed_along_r_new = scalar_x_vector(-1, speed_along_r)
     
     i = gr.Point(1, 0)
     j = gr.Point(0, 1)
@@ -71,39 +88,44 @@ def changed_speed(speed, x, y):
     speed = gr.Point(speed_x, speed_y)
     return speed
 
-def vector_along_radius(x,y):
-    length_vector = sqrt( (x_0 - x)**2 + (y_0 - y)**2 )
-    vector_along_r = gr.Point( (x_0 - x)/length_vector , (y_0 - y)/length_vector )
+
+def vector_along_radius(x, y):
+    length_vector = sqrt((x_0-x)**2 + (y_0-y)**2)
+    vector_along_r = gr.Point(
+        (x_0-x) / length_vector,
+        (y_0-y) / length_vector
+    )
     
     return vector_along_r
 
 
 def vector_normal_radius(x,y):
-    length_vector = sqrt( (x_0 - x)**2 + (y_0 - y)**2 )
-    vector_normal_r = gr.Point( (y_0 - y)/length_vector , -(x_0 - x)/length_vector )
+    length_vector = sqrt((x_0 - x)**2 + (y_0 - y)**2 )
+    vector_normal_r = gr.Point((y_0-y)/length_vector, - (x_0-x)/length_vector)
     
     return vector_normal_r
     
 
 def check_position(speed, x, y):
-    if (x_0 - x)**2 + (y_0 - y)**2 >= 260**2:
+    if (x_0-x)**2 + (y_0-y)**2 >= 260**2:
         speed = changed_speed(speed, x, y)
         
     return speed
         
-    
-while True:
-    ball_go()
 
-    position = add(position, speed) 
+
+draw_big_circle()
+ball1 = draw_ball()
+
+while True:
+    ball_go(ball1)
+
+    position = add(position,speed) 
     
     x = position.x
     y = position.y
-    
-    #Абсолютно неясно, почему не работает прога, ежели я вместо строки ниже напишу что-нибудь like this: 
-    # check_position(speed, x, y)  !!! при условии, что в функции не будет return. That is the question...
-    
-    speed = check_position(speed, x, y)
+        
+    speed = check_position(speed,x,y)
      
     
     gr.time.sleep(0.02)
